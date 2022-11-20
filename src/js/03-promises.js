fe36a6136415a3d29c8dd26c1c0c967475740246
ZanderOl <olexandruk@gmail.com>
2022-11-20 21:10:08 +0200

    Promises done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  let delay = Number(form.elements.delay.value);
+  let step = Number(form.elements.step.value);
+  let amount = Number(form.elements.amount.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise({ i, delay })
+      .then(({ i, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${i + 1} in ${delay}ms`);
+      })
+      .catch(({ i, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${i + 1} in ${delay}ms`);
+      });
+    delay += step;
   }
+}
+
+function createPromise({ position, delay }) {
+  const shouldResolve = Math.random() > 0.3;
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(delay);
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
