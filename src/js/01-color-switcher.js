0b9f4b1586873c4ac5838c803b48fcbc69e6b986
ZanderOl <olexandruk@gmail.com>
2022-11-20 19:16:14 +0200

    Change body color done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let intervalId;
+btnStop.disabled = true;
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function changeBtnStatus() {
+  btnStart.disabled = !btnStart.disabled;
+  btnStop.disabled = !btnStop.disabled;
+}
+
+function onBtnStartClick() {
+  intervalId = setInterval(changeBodyColor, 1000);
+  changeBtnStatus();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onBtnStopClick() {
+  clearInterval(intervalId);
+  changeBtnStatus();
+}
